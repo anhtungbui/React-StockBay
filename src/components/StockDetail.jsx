426d74819723946e98092e7ff783704e503fb138
Anh Tung Bui <anhtung.bui@gmail.com>
2021-02-13 19:33:37 +0100

    Add feature: Stock price history

--- src/components/StockDetail.jsx
+++ src/components/StockDetail.jsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Highcharts from 'highcharts';
+import HighStock from 'highcharts/highstock';
+import HighchartsReact from 'highcharts-react-official';
 import ProfileList from './ProfileList';
 
 const FINNHUB_TOKEN = process.env.REACT_APP_FINNHUB_TOKEN;
-
-const prepareChartData = (data) => {
-    console.log(data);
-};
 
 export default function StockDetail({ match }) {
     const symbol = match.params.symbol;
     const [profile, setProfile] = useState([]);
     const [priceData, setPriceData] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [chartData, setChartData] = useState(null);
 
     useEffect(() => {
         getCompanyProfile();
@@ -43,26 +43,71 @@ export default function StockDetail({ match }) {
                 params: {
                     symbol: symbol,
                     resolution: 'D',
-                    from: Date.parse('2021-02-01T00:00:00') / 1000,
+                    from: Date.parse('2020-02-13T00:00:00') / 1000,
                     to: new Date().getTime(),
                     token: FINNHUB_TOKEN,
                 },
             }
         );
         // console.log(data);
-        prepareChartData(data);
+
+        let chartData = await prepareChartData(data);
+        setChartData(chartData);
+        // console.log(chartData);
         // setNews(data);
+    };
+
+    function prepareChartData(data) {
+        // console.log('Hello');
+        // console.log(data);
+        const chartData = [];
+        for (let i = 0; i < data.t.length; i++) {
+            chartData.push([
+                data.t[i] * 1000,
+                parseFloat(data.c[i].toFixed(2)),
+            ]);
+        }
+        return chartData;
+    }
+
+    const options = {
+        title: {
+            text: `${profile.name} (${profile.ticker})`,
+        },
+        // chart: {
+        //     type: 'spline',
+        // },
+        rangeSelector: {
+            selected: 1,
+        },
+        series: [
+            {
+                name: 'Price in ' + profile.currency,
+                data: chartData,
+                tooltip: {
+                    valueDecimals: 2,
+                },
+            },
+        ],
     };
 
     return (
         <div className="row mt-3">
-            <div className="col-4">
+            <div className="col-lg-4 order-2">
                 <ProfileList profile={profile} loading={loading} />
             </div>
-            <div className="col-8">
+            <div className="col-lg-8 order-1 mb-3">
                 <div className="card">
                     <div className="card-header">Stock Price</div>
-                    <div className="card-body"></div>
+                    <div className="card-body">
+                        {chartData && (
+                            <HighchartsReact
+                                highcharts={HighStock}
+                                constructorType={'stockChart'}
+                                options={options}
+                            />
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
